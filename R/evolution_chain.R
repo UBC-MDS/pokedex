4f151fbbe2c0d313806c73c1ad04e0b777a99f93
Jordan Dubchak <jordandubchak@gmail.com>
2018-04-12 10:05:43 -0700

    Add evolution chain documentation

---
+++ R/evolution_chain.R
@@ -0,0 +1,13 @@
+#' Evolution chain IDs of pokemon characters on pokeapi V2.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 415 rows and 2 variables:
+#' \describe{
+#'   \item{id}{id, used for lookup in API}
+#'   \item{name}{name of pokemon character}
+#' }
+#'
+#' @usage data(evolution_chain)
+"evolution_chain"
