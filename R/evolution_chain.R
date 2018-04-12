86235cfd7a12e02ea41cb2cf3547975af4a87a31
Jordan Dubchak <jordandubchak@gmail.com>
2018-04-12 10:41:43 -0700

    Update evolution chain R script

--- R/evolution_chain.R
+++ R/evolution_chain.R
@@ -1,8 +1,5 @@
 #' Evolution chain IDs of pokemon characters on pokeapi V2.
 #'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
-#'
 #' @format A data frame with 415 rows and 2 variables:
 #' \describe{
 #'   \item{id}{id, used for lookup in API}
@@ -10,4 +7,4 @@
 #' }
 #'
 #' @usage data(evolution_chain)
-"evolution_chain"
+"evolution_chain_ids"
