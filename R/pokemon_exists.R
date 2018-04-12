16640bde99450ea1dacedacd31aa43457fbadadc
Jordan Dubchak <jordandubchak@gmail.com>
2018-04-12 11:15:22 -0700

    Update readme and pokemon_exists

--- R/pokemon_exists.R
+++ R/pokemon_exists.R
@@ -7,10 +7,9 @@
 #'
 #' @export
 pokemon_exists <- function(name_val){
-  tryCatch({
-         data("evolution_chain")
-         evolution_chain_ids$name <- as.character(evolution_chain_ids$name)
-  })
+
+  data("evolution_chain")
+  evolution_chain_ids$name <- as.character(evolution_chain_ids$name)
 
   if(!is.character(name_val)){
         return(FALSE)
