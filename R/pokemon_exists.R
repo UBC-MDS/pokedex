e350c5ab30795248e62e2c4f33b5dcce6e6322b9
Jordan Dubchak <jordandubchak@gmail.com>
2018-04-12 10:41:04 -0700

    Update pokemon_exists function with new data file

--- R/pokemon_exists.R
+++ R/pokemon_exists.R
@@ -3,18 +3,18 @@
 #' @title Test pokemon exists
 #' @param name Name of pokemon
 #' @return Boolean TRUE if the pokemon exists in the API, FALSE otherwise
-#'
 #' @importFrom magrittr "%>%"
 #'
 #' @export
-pokemon_exists <- function(name){
+pokemon_exists <- function(name_val){
   tryCatch({
-         lookup <- pokedex:::lookup
+         data("evolution_chain")
+         evolution_chain_ids$name <- as.character(evolution_chain_ids$name)
   })
 
-  if(!is.character(name)){
+  if(!is.character(name_val)){
         return(FALSE)
-  }else if(!tolower(name) %in% lookup$name){
+  }else if(!tolower(name_val) %in% evolution_chain_ids$name){
         return(FALSE)
   }else{
         return(TRUE)
